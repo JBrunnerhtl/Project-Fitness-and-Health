bfeb31f070f0723decdc254cc954e17ae3425774
LParzerhtl <l.parzer@students.htl-leonding.ac.at>
2025-02-03 20:54:21 +0100

    in the new created javasript file I added a code that takes the information or inputs of form and stores them into const variables

---
+++ fitness-and-health-website/shop.js
@@ -0,0 +1,12 @@
+document.addEventListener('DocumentContentLoaded', () =>{  // dom takes a look that the code is runed if the html file is loaded
+
+    const form = document.querySelector('form');// it searches for the first css element off form and stores it in the const form.
+
+    form.addEventListener('submit', (event) => { // adds an event listener to the form that waits for a submit event
+        event.preventDefault(); // prevent the reload of the page, it should run smooth
+        const problem = document.getElementById('problem').value;
+        const maxPrice = document.getElementById('max-price').value;
+        const category = document.getElementById('category').value;
+
+    })
+});
