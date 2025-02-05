a1ad2b8b2fec529c96004c5141f35484dd92bd30
LParzerhtl <l.parzer@students.htl-leonding.ac.at>
2025-02-05 20:00:56 +0100

    all inputs of the formular in training will be stored in these consts

---
+++ fitness-and-health-website/training.js
@@ -0,0 +1,13 @@
+document.addEventListener('DocumentContentLoaded', () =>{
+    const submitButton = document.querySelector(".btn-custom");
+
+    submitButton.addEventListener("click", function () {
+        const weight = parseFloat(document.getElementById("weight").value);
+        const height = parseFloat(document.getElementById("height").value);
+        const age = parseInt(document.getElementById("age").value, 10);
+        const gender = document.getElementById("gender").value;
+        const activityLevel = document.getElementById("activity-level").value;
+        const disabilities = document.getElementById("disabilities").value;
+        const fitnessGoal = document.getElementById("fitness-goal").value;
+    })
+}
