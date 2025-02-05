44b416ede7c70655fb56e2bbf0d38cb44bcc56d8
LParzerhtl <l.parzer@students.htl-leonding.ac.at>
2025-02-05 20:35:18 +0100

    added a record or idk a const for all data of the user to collect it

--- fitness-and-health-website/training.js
+++ fitness-and-health-website/training.js
@@ -2,6 +2,7 @@ document.addEventListener('DocumentContentLoaded', () =>{
     const submitButton = document.querySelector(".btn-custom");
 
     submitButton.addEventListener("click", function () {
+
         const weight = parseFloat(document.getElementById("weight").value);
         const height = parseFloat(document.getElementById("height").value);
         const age = parseInt(document.getElementById("age").value, 10);
@@ -9,5 +10,15 @@ document.addEventListener('DocumentContentLoaded', () =>{
         const activityLevel = document.getElementById("activity-level").value;
         const disabilities = document.getElementById("disabilities").value;
         const fitnessGoal = document.getElementById("fitness-goal").value;
+        const USER_DATA = {
+            weight,
+            height,
+            age,
+            gender,
+            activityLevel,
+            disabilities,
+            fitnessGoal
+        };
     })
-}
+
+})
