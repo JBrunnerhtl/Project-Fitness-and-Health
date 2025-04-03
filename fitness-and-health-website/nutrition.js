6f00cf791d9327af7f49b54524337488b765a4f7
JBrunnerhtl <j.brunner1@students.htl-leonding.ac.at>
2025-04-03 18:43:51 +0200

    changed some things in the nutrition.js and nutrition.html

--- fitness-and-health-website/nutrition.js
+++ fitness-and-health-website/nutrition.js
@@ -1,10 +1,10 @@
 
-const apiKey = ""; // Ersetze dies mit deinem echten API-Schlüssel
-const query = 50; // Suchbegriff für das Rezept
+const apiKey = "824f5b8c655b400a99d93a9f4e841ed0"; // Ersetze dies mit deinem echten API-Schlüssel
+let proteins = document.getElementById('weight') *1.6; // Suchbegriff für das Rezept
 const header = "recipes";
 
 async function getRecipesForProtein() {
-    const url = `https://api.spoonacular.com/${header}/complexSearch?maxProtein=${query}&apiKey=${apiKey}`;
+    const url = `https://api.spoonacular.com/${header}/complexSearch?maxProtein=${proteins}&apiKey=${apiKey}`;
 
     try {
         const response = await fetch(url);
@@ -13,10 +13,14 @@ async function getRecipesForProtein() {
         }
         const data = await response.json();
         console.log(data); // Rezepte ausgeben
+
     } catch (error) {
         console.error("API-Fehler:", error);
     }
 }
+
+getRecipesForProtein().then();
+
 
 async function getRecipesForCarbs() {
 
