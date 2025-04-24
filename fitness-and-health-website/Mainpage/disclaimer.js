7503dc053e98f7aa99fc3a655073dd4172a35670
LParzerhtl <l.parzer@students.htl-leonding.ac.at>
2025-04-24 19:32:41 +0200

    added a darkmode switcher. It is not finished. It is just the beginning

--- fitness-and-health-website/Mainpage/disclaimer.js
+++ fitness-and-health-website/Mainpage/disclaimer.js
@@ -45,4 +45,36 @@ document.addEventListener('DOMContentLoaded', function() {
         disclaimerModal.hide();
     });
 
+
+
+});
+
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+    const isDarkMode = document.body.classList.contains('dark-mode');
+    localStorage.setItem('darkMode', isDarkMode);
+
+
+    const icon = document.getElementById('darkModeIcon');
+    const text = document.getElementById('darkModeText');
+
+    if (isDarkMode) {
+        icon.src = "../icones/bx--sun.png";
+        text.textContent = "Light Mode";
+    } else {
+        icon.src = "../icones/bx--moon.png";
+        text.textContent = "Dark Mode";
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    if (localStorage.getItem('darkMode') === 'true') {
+        document.body.classList.add('dark-mode');
+        document.getElementById('darkModeIcon').src = "../icones/bx--moon";
+        document.getElementById('darkModeText').textContent = "Light Mode";
+    }
+
+
+    document.getElementById('darkModeToggle').addEventListener('click', toggleDarkMode);
 });
