8eb845dc74602ede6a0501d60865d5dabb6686cc
LParzerhtl <l.parzer@students.htl-leonding.ac.at>
2025-05-09 09:46:23 +0200

    installed env and tried to work on the auth0 problem

--- fitness-and-health-website/All/Auth/auth0.js
+++ fitness-and-health-website/All/Auth/auth0.js
@@ -1,4 +1,4 @@
-const { auth } = require('express-openid-connect');
+/*const { auth } = require('express-openid-connect');
 
 const config = {
     authRequired: false,
@@ -15,4 +15,15 @@ app.use(auth(config));
 // req.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
     res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
+});*/
+
+const { auth } = require('express-oauth2-jwt-bearer');
+app.use(
+    auth({
+        issuerBaseURL: 'https://dev-mxq3wnna1k2lh5ot.us.auth0.com',
+        audience: 'fitness-and-health-api',
+        secret: 'FiHWZ1JcY6bf-3cpbFHVyRnM93lfYTeVm4xi2o3T5Frjr_llpaLDp1acfhtMgQuj',
+        tokenSigningAlg: 'HS256',
+    })
+);
+
