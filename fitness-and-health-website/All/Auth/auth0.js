115a538e792670395968f45984e6d817508106b4
LParzerhtl <l.parzer@students.htl-leonding.ac.at>
2025-05-13 08:34:24 +0200

    try to make the auth0 working

--- fitness-and-health-website/All/Auth/auth0.js
+++ fitness-and-health-website/All/Auth/auth0.js
@@ -1,29 +1,27 @@
-/*const { auth } = require('express-openid-connect');
+document.addEventListener("DOMContentLoaded", async () => {
+    const auth0 = await createAuth0Client({
+        domain: "dev-mxq3wnna1k2lh5ot.us.auth0.com",
+        client_id: "X57y86qMIx3CaxUgYzMcSNOB8fxkRLAX",
+        authorizationParams: {
+            redirect_uri: window.location.origin,
+            audience: "fitness-and-health-api" // falls du APIs schützt
+        }
+    });
 
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: 'FiHWZ1JcY6bf-3cpbFHVyRnM93lfYTeVm4xi2o3T5Frjr_llpaLDp1acfhtMgQuj',
-    baseURL: 'http://localhost:3000',
-    clientID: 'X57y86qMIx3CaxUgYzMcSNOB8fxkRLAX',
-    issuerBaseURL: 'https://dev-mxq3wnna1k2lh5ot.us.auth0.com'
-};
 
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
+    const loginButton = document.getElementById("login-button");
+    loginButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        auth0.loginWithRedirect();
+    });
 
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});*/
 
-const { auth } = require('express-oauth2-jwt-bearer');
-app.use(
-    auth({
-        issuerBaseURL: 'https://dev-mxq3wnna1k2lh5ot.us.auth0.com',
-        audience: 'fitness-and-health-api',
-        secret: 'FiHWZ1JcY6bf-3cpbFHVyRnM93lfYTeVm4xi2o3T5Frjr_llpaLDp1acfhtMgQuj',
-        tokenSigningAlg: 'HS256',
-    })
-);
+    if (window.location.search.includes("code=") && window.location.search.includes("state=")) {
+        await auth0.handleRedirectCallback();
+        window.history.replaceState({}, document.title, "/");
+    }
 
+
+    const isAuthenticated = await auth0.isAuthenticated();
+    console.log("Eingeloggt:", isAuthenticated);
+});
