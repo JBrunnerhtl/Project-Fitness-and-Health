3d3f26af6a62bf664010ec6fa0a193e663282eb2
ErnadMusic-HTL <e.music1@students.htl-leonding.ac.at>
2025-05-16 08:09:20 +0200

    login works fully now

--- fitness-and-health-website/All/Auth/auth0.js
+++ fitness-and-health-website/All/Auth/auth0.js
@@ -1,5 +1,10 @@
-
-import { auth0Config } from './config.js';
+const auth0Config = {
+    domain: "dev-3hh4wwvbhoxgpjpc.us.auth0.com",
+    clientId: "41JRf64iWCFy2UTpsWAjIXqmVEB9fV40",
+    audience: undefined,
+    redirectUri:"http://localhost:5500/fitness-and-health-website/Mainpage/",
+    logoutRedirectUri: "http://localhost:5500/fitness-and-health-website/Mainpage/"
+};
 
 let auth0Client;
 const LOGIN_BUTTON_ID = "login-button";
@@ -153,6 +158,7 @@ const performLogout = (event) => {
 
 
 const mainAuth = async () => {
+    console.log("Nega")
     try {
         await initializeAuth0Client();
         await handleAuth0Redirect();
--- fitness-and-health-website/All/Auth/config.js
+++
@@ -1,7 +0,0 @@
-export const auth0Config = {
-    domain: "dev-3hh4wwvbhoxgpjpc.us.auth0.com",
-    clientId: "41JRf64iWCFy2UTpsWAjIXqmVEB9fV40",
-    audience: undefined,
-    redirectUri:"http://localhost:63342/Project-Fitness-and-Health/fitness-and-health-website/Mainpage/index.html",
-    logoutRedirectUri: "http://localhost:63342/Project-Fitness-and-Health/fitness-and-health-website/Mainpage/index.html"
-};
