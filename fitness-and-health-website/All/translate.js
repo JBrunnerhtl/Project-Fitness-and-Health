44bbad1a889e4d96d669d29e7f3d76fc5322a097
LParzerhtl <l.parzer@students.htl-leonding.ac.at>
2025-06-12 15:03:46 +0200

    the pages translates automatically if you change the language once, before every page you have to do it on your own

--- fitness-and-health-website/All/translate.js
+++ fitness-and-health-website/All/translate.js
@@ -73,8 +73,8 @@ async function translateTexts(texts, sourceLang, targetLang) {
 
     return translationMap;
 }
-/*
+
 document.addEventListener('DOMContentLoaded', () => {
     const savedLanguage = localStorage.getItem('language') || 'de';
     setLanguage(savedLanguage);
-});*/
+});
